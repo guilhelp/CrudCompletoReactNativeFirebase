7e11b9aa25ca1dd79af17a45163f9adbcc60bd18
guilhelp <guilherme.paula.dev@gmail.com>
2023-06-03 17:05:20 -0300

    crud

---
+++ src/telas/DadosProduto/estilos.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems: "flex-end",
+        justifyContent: "flex-start",
+        padding: 25,
+        
+    }
+})
